f7970b567137aa1c7dc6d6e0c4d55636ff0dfa04
이명규 <082137@daum.net>
2023-11-06 21:37:28 +0900

    초기화

---
+++ iam/src/main/java/com/f7dec8/iam/IamApplication.java
@@ -0,0 +1,13 @@
+package com.f7dec8.iam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IamApplication.class, args);
+	}
+
+}
---
+++ iam/src/test/java/com/f7dec8/iam/IamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.f7dec8.iam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
