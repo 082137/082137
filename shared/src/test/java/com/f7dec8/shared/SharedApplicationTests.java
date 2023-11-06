a19e7bbc81ab0c0305bcda73b7212eca5bf85d3c
이명규 <082137@daum.net>
2023-11-06 22:11:27 +0900

    shared 추가

---
+++ shared/src/test/java/com/f7dec8/shared/SharedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.f7dec8.shared;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
